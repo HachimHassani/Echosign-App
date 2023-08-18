5bc13faec3092e736e782e1c87a2d705596b4005
Akashioreki12 <akramtalibi1902@gmail.com>
2023-08-18 12:55:38 +0100

    some updates

--- app/Echosign/app/index.js
+++ app/Echosign/app/index.js
@@ -1,24 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Link } from 'expo-router';
+import welcome from './(welcome)/welcome';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>main page!</Text>
-      <Link href="/auth/login">login</Link>
-
-  <Link href="/test">Test</Link>
+    <View className="flex-1 bg-cyan-200 items-center justify-center">
+      <Text className="text-xl">main page!</Text>
+      <Link href="/login" className="text-blue-500">login</Link>
+      <Link href="/screens/welcome" className="text-blue-500">welcome</Link>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  // You can keep this empty for now or customize as needed
 });

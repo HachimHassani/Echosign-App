abfc9a4269adef1a6fd5e0b5c6e86a3919dc5e7b
unknown <38157591+HachimHassani@users.noreply.github.com>
2023-10-01 21:23:07 +0100

    modified few bits

--- app/Echosign/app/index.js
+++ app/Echosign/app/index.js
@@ -18,68 +18,7 @@ export default function Index() {
   // Amplify.register(Predictions);
   // Predictions.addPluggable(new AmazonAIPredictionsProvider());
   return (
-<<<<<<< HEAD
-    <>
-      <Link
-        href="/welcome"
-        className=" text-blue-500 justify-center items-center text-xl"
-      >
-        start
-      </Link>
-      <Link
-        href="/mainPage"
-        className=" text-blue-500 justify-center items-center text-xl"
-      >
-        home Page
-      </Link>
-      <Link
-        href="/courses"
-        className=" text-blue-500 justify-center items-center text-xl"
-      >
-        learn
-      </Link>
-      <Link
-        href="/lessonABC"
-        className=" text-blue-500 justify-center items-center text-xl"
-      >
-        study abc
-      </Link>
-      <Link
-        href="/echoSign"
-        className=" text-blue-500 justify-center items-center text-xl"
-      >
-        about us
-      </Link>
-      <Link
-        href="/lessonColor"
-        className=" text-blue-500 justify-center items-center text-xl"
-      >
-        Study color
-      </Link>
-      <Link
-        href="/lessonAnimals"
-        className=" text-blue-500 justify-center items-center text-xl"
-      >
-        Study animals
-      </Link>
-      <Link
-        href="/lessonNum"
-        className=" text-blue-500 justify-center items-center text-xl"
-      >
-        Study numbers
-      </Link>
-      <Link
-        href="/game"
-        className=" text-blue-500 justify-center items-center text-xl"
-      >
-        practice
-      </Link>
-    </>
-=======
-
-
     <Redirect href="/welcome" /> 
 
->>>>>>> fc600dcff1de67f987c953a365cc27a7f206016a
   );
 }
